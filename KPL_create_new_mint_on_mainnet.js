450ff28d9bbf3c379bfa9fc3315426fa3e503e1d
herman124410 <herman@koii.network>
2024-12-16 13:53:01 -0400

    Fix a dotenv issue

--- KPL_create_new_mint_on_mainnet.js
+++ KPL_create_new_mint_on_mainnet.js
@@ -1,10 +1,11 @@
 import { createMint } from "@solana/spl-token";
 import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js";
+import dotenv from "dotenv";
+dotenv.config();
 const privateKeyArray = process.env.PRIVATE_KEY.split(",").map(Number);
 const payer = Keypair.fromSecretKey(Uint8Array.from(privateKeyArray));
 const mintAuthority = Keypair.fromSecretKey(Uint8Array.from(privateKeyArray));
 const freezeAuthority = Keypair.fromSecretKey(Uint8Array.from(privateKeyArray));
-
 const connection = new Connection(
   /***********DO NOT EDIT ABOVE THIS LINE***********/
   "http://localhost:8899", // Change to mainnet url
